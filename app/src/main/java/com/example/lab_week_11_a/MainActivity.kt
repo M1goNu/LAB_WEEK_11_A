30fb786bddf4f167afb400074067269f8605c117
M1goNu <rifqi.aldino71@gmail.com>
2025-11-23 11:04:13 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

--- app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.lab_week_11_a
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_11_a.dataStore.SettingsApplication
+import com.example.lab_week_11_a.dataStore.SettingsViewModel
+import com.example.lab_week_11_a.sharedPreference.PreferenceApplication
+import com.example.lab_week_11_a.sharedPreference.PreferenceViewModel
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        //stored data used SharedPreference
+        //val preferenceWrapper = (application as PreferenceApplication).preferenceWrapper
+        //val preferenceViewModel = ViewModelProvider(this, object :
+            //ViewModelProvider.Factory {
+            //override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                //return PreferenceViewModel(preferenceWrapper) as T
+            //}
+        //})[PreferenceViewModel::class.java]
+        //preferenceViewModel.getText().observe(this
+        //) {
+            //findViewById<TextView>(
+                //R.id.activity_main_text_view
+            //).text = it
+        //}
+
+        // Stored data used dataStore
+        val preferenceWrapper = (application as SettingsApplication).settingsStore
+        // Create the view model instance with the preference wrapper as the constructor parameter
+        // To pass the preference wrapper to the view model, we need to use a view model factory
+        val preferenceViewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SettingsViewModel(preferenceWrapper) as T
+            }
+        })[SettingsViewModel::class.java]
+
+        // Observe the text live data
+        preferenceViewModel.textLiveData.observe(this) {
+            // Update the text view when the text live data changes
+            findViewById<TextView>(
+                R.id.activity_main_text_view
+            ).text = it
+        }
+        findViewById<Button>(R.id.activity_main_button).setOnClickListener {
+            // Save the text when the button is clicked
+            preferenceViewModel.saveText(
+                findViewById<EditText>(R.id.activity_main_edit_text)
+                    .text.toString()
+            )
         }
     }
 }
